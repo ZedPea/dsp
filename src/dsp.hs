9cb9b390fc7e4516562cb4949d7cb9e36c1338ea
ZedPea <z.palmer@openmailbox.org>
2017-05-25 19:35:49 +0100

    Improve code, add more effects

--- src/dsp.hs
+++ src/dsp.hs
@@ -1,13 +1,17 @@
-module Dsp where
+{- a lot of the code in this is inefficient, and just for messing around
+with different effects. Specifically vector -> list -> vector is very
+inefficient. Some of these sound absolutely disgusting. Might want to turn
+down your volume! -}
 
-import System.Exit (ExitCode)
+import Prelude hiding (readFile, take, reverse)
+import System.Exit
+import Foreign.Storable
+import Text.Printf
+import Control.Monad
 import Sound.Sox.Play
-import Sound.Sox.Frame (C(..))
 import Sound.Sox.Option.Format
 import Sound.File.Sndfile
 import Sound.File.Sndfile.Buffer.StorableVector
-import qualified Data.StorableVector as V
-import qualified Data.StorableVector.Lazy as LazyV
 import Synthesizer.Generic.Cut
 import Synthesizer.Generic.Filter.NonRecursive
 import Synthesizer.Generic.Filter.Recursive.Comb
@@ -15,39 +19,84 @@ import Synthesizer.Generic.Filter.Delay
 import Synthesizer.Generic.Signal
 import Synthesizer.Generic.Analysis
 import Synthesizer.Generic.Noise
-import qualified Synthesizer.Generic.Displacement as Distort
 import Synthesizer.Basic.Distortion
 import qualified Synthesizer.Generic.Loop as Loop
-import Foreign.Storable
+import qualified Synthesizer.Generic.Displacement as Distort
 import qualified Control.Concurrent.Thread.Delay as Delay
-import Data.List (sort)
-import Text.Printf
-import Control.Monad
-import Prelude hiding (readFile, take, negate, reverse)
+import qualified Data.StorableVector as V
+import qualified Data.StorableVector.Lazy as LazyV
 
-foo :: IO (LazyV.Vector Double)
-foo = do
-    (info, Just test) <- readFile "test2.flac"
-    let snippet = take (numSeconds info 11) (toLazy $ fromBuffer test)
-    return snippet
+main :: IO ()
+main = do
+    (info, Just test1) <- readFile "SmokeOnTheWater.ogg"
+    (_, Just test2) <- readFile "FallenDownTheWire.ogg"
+    (_, Just test3) <- readFile "Normal.ogg"
 
-simplePlay :: (Storable a, Sound.Sox.Frame.C a) => LazyV.Vector a -> IO ExitCode
-simplePlay = simple LazyV.hPut (numberOfChannels 2) 88200
+    printf "The file has %d frames, a sample rate of %d, %d channels, \
+            \%d sections, and lasts for %f seconds\n" (frames info)
+            (samplerate info) (channels info) (sections info) (duration info)
 
-lazySort :: (Ord a, Storable a) => LazyV.Vector a -> LazyV.Vector a
-lazySort file = LazyV.pack LazyV.defaultChunkSize . sort $ LazyV.unpack file
+    let snippet = take tenSeconds (toLazy $ fromBuffer test1)
+        snippet2 = take tenSeconds (toLazy $ fromBuffer test2)
+        snippet3 = take tenSeconds (toLazy $ fromBuffer test3)
+        tenSeconds = numSeconds info 10.0
 
-testlol :: (Fractional y, Ord y, Storable y) => LazyV.Vector y -> LazyV.Vector y
-testlol = LazyV.map (\x -> if x < 0.02 then 0 else x)
+        playEffect' f msg = playEffect snippet f msg info
 
-testlol2 :: (Fractional y, Ord y, Storable y) => LazyV.Vector y -> LazyV.Vector y
-testlol2 = LazyV.map (\x -> if x > 0.05 then x+0.05 else x)
+        fadeLoop = take tenSeconds . Loop.fade (0.5 :: Double) 
+                   (numSeconds info 2) (numSeconds info 2)
 
-double2float :: Double -> Float
-double2float = realToFrac
+        loop = take tenSeconds . Loop.simple (numSeconds info 1)
+               (numSeconds info 3)
 
-range :: (Ord a, Storable a, Num a) => LazyV.Vector a -> a
-range x = LazyV.maximum x - LazyV.minimum x
+        downSample x s = simple LazyV.hPut (numberOfChannels 2)
+                         (round $ 88200 * (1/ fromIntegral x)) 
+                         (downsample (LazySize 1) x s)
+
+        --fade in time, normal volume time, fade out time
+        fade = fadeInOut (numSeconds info 3) (numSeconds info 5)
+                         (numSeconds info 3)
+
+        distort = amplify 0.05 . Distort.distort powerSigned
+                  (LazyV.repeat LazyV.defaultChunkSize 0)
+
+        noisy s = LazyV.map (\x -> if x > average s then x + 0.1 else x - 0.1) s
+
+        actions = [(id, "Original file..."),
+                   (downsample2 (LazySize 1), "Speed up..."),
+                   (eightbit, "Rounded amplitude range..."),
+                   (removeSmallChanges, "Remove small changes in amplitude..."),
+                   (boostBigChanges, "Increase large changes in amplitude..."),
+                   (mix (static (numSeconds info 0.1) snippet), "Delay..."),
+                   (LazyV.map clip', "Clipped..."),
+                   (\x -> Distort.mixMulti [x, snippet2, snippet3], 
+                    "Mix multi songs"),
+                   (distort, "Distortion..."),
+                   (fadeLoop, "Fade loop"),
+                   (fade, "Fade..."),
+                   (amplify 0.1, "Decrease volume..."),
+                   (envelope (white $ LazySize 1), "Splice with white noise"),
+                   (loop, "Loop"),
+                   (reverse, "Reversed"),
+                   (mix snippet2, "Mixed with another track..."),
+                   (run (numSeconds info 1) (1 :: Double), "Echoes..."),
+                   (reverse' info, "Half reverse..."),
+                   (noisy, "Noisy...")
+                  ]
+
+    mapM_ (uncurry playEffect') actions
+
+    {- can't do this with playEffect' because sample rate when playing needs
+    to be decreased so it plays at normal speed -}
+    void $ putStrLn "Downsample..." >> downSample 16 snippet
+
+removeSmallChanges :: (Fractional y, Ord y, Storable y) => LazyV.Vector y
+                                                        -> LazyV.Vector y
+removeSmallChanges = LazyV.map (\x -> if x < 0.02 then 0 else x)
+
+boostBigChanges :: (Fractional y, Ord y, Storable y) => LazyV.Vector y 
+                                                     -> LazyV.Vector y
+boostBigChanges = LazyV.map (\x -> if x > 0.05 then x+0.05 else x)
 
 eightbit :: LazyV.Vector Double -> LazyV.Vector Double
 eightbit file = LazyV.map (\x -> (fromIntegral x / 16) - min') ints
@@ -55,107 +104,7 @@ eightbit file = LazyV.map (\x -> (fromIntegral x / 16) - min') ints
           normalized = LazyV.map (+ min') file
           min' = LazyV.minimum file
 
-uniqueValues' :: Eq a => [a] -> [a] -> [a]
-uniqueValues' [] acc = acc
-uniqueValues' (x:xs) acc
-    | x `elem` acc = uniqueValues' xs acc
-    | otherwise = uniqueValues' xs (x:acc)
-
-uniqueValues :: (Eq a, Storable a) => LazyV.Vector a -> LazyV.Vector a
-uniqueValues xs = LazyV.pack LazyV.defaultChunkSize $ uniqueValues' (LazyV.unpack xs) []
-
-lowerSampleRate :: (Num t1, Eq t1) => t1 -> t1 -> [t] -> [t]
-lowerSampleRate _ _ [] = []
-lowerSampleRate n initial (x:xs)
-    | n == 0 = x : lowerSampleRate initial initial xs
-    | otherwise = lowerSampleRate (n-1) initial xs
-
-playAtLowerSampleRate :: (RealFrac a1, Storable a, C a) => a1 -> LazyV.Vector a -> IO ExitCode
-playAtLowerSampleRate n sample = do
-    let fixedSample = LazyV.pack LazyV.defaultChunkSize $ lowerSampleRate (n - 1) (n - 1) (LazyV.unpack sample)
-    simple LazyV.hPut (numberOfChannels 2) (round $ 88200 * (1 / n)) fixedSample
-
-main :: IO ()
-main = do
-    (info, Just test1) <- readFile "test3.flac"
-    (_, Just test2) <- readFile "test2.flac"
-    (_, Just test3) <- readFile "test.flac"
-    (_, Just test4) <- readFile "test4.flac"
-
-    printf "The file has %d frames, a sample rate of %d, %d channels, \
-            \%d sections, and lasts for %f seconds\n" (frames info)
-            (samplerate info) (channels info) (sections info) (duration info)
-
-    let snippet = take (numSeconds info 11) (toLazy $ fromBuffer test1)
-        snippet2 = take (numSeconds info 11) (toLazy $ fromBuffer test2)
-        snippet3 = take (numSeconds info 11) (toLazy $ fromBuffer test3)
-        snippet4 = take (numSeconds info 11) (toLazy $ fromBuffer test4)
-
-    let playEffect' f msg = playEffect snippet f msg info
-    let playSnippet snippet' = play snippet' info
-
-    let wooLong = LazyV.take (LazyV.length snippet4 * 20) $ LazyV.cycle snippet4
-
-    let wooplusWhite = LazyV.zipWith (+) wooLong (white $ LazySize 8)
-
-    let minisnippet = take 88200 $ LazyV.drop 88200 snippet2
-
-    print minisnippet
-
-    playSnippet minisnippet
-
-    playEffect' (LazyV.map clip') "Noisy..."
-
-    playEffect wooLong id "woo long" info
-
-    playEffect wooplusWhite id "woo + white noise" info
-
-    playEffect wooLong (LazyV.map (powerSigned 4)) "Clipped woo!" info
-
-    playEffect' (\x -> amplify 10 $ envelopeVector x wooLong) "Envelope..."
-
-    playEffect' (\x -> Distort.mixMulti [x, snippet2, snippet3]) "Mix multi songs..."
-
-    playEffect' (Distort.distort powerSigned snippet2) "Distortion..."
-
-    playEffect' (take (numSeconds info 11) . Loop.fade (0.5 :: Double) (numSeconds info 2) (numSeconds info 2)) "Fade loop..."
-
-    playEffect' id "Original file..."
-
-    -- fade in time, normal volume time, fade out time
-    playEffect' (fadeInOut (numSeconds info 3) (numSeconds info 5) 
-                (numSeconds info 3)) "Fade..."
-
-    playEffect' (downsample2 (LazySize 8)) "This is apparently a downsample, I\
-                                           \ must be doing it wrong. It sounds\
-                                           \ funny though..."
-
-    playEffect' (amplify 0.1) "Negative amplify..."
-
-    playEffect' (envelope (white $ LazySize 8)) 
-                "Splicing with some disgusting white noise..."
-
-    playEffect' (take (numSeconds info 11) . Loop.simple (numSeconds info 1)
-                (numSeconds info 3)) "Loop..."
-
-    playEffect' reverse "Reversed..."
-
-    playEffect' (mix snippet2) "Mixed..."
-
-    playEffect' (run (numSeconds info 1) (1 :: Double)) "Echoes..."
-
-    playEffect' (reverse' info) "Reversed sorta"
-
-    playEffect' (LazyV.zipWith (+) snippet2) "Double...?"
-
-    let avg = average snippet
-    playEffect' (LazyV.map (\x -> if x > avg then x + 0.1 else x - 0.1)) "Noisy..."
-
-    let newSnippet = static (numSeconds info 0.5) snippet
-    playEffect' (mix newSnippet) "Delay..."
-        
-
-{- this is VERY bad - it's inefficent as hell, and there's got to be a better
+{- this is bad - it's inefficent as hell, and there's got to be a better
 way! fromBuffer returns a Data.StorableVector, but play wants a
 Data.StorableVector.Lazy. For now this is a quick hack, but if performance
 starts being needed, you should definitely fix this -}
@@ -170,7 +119,8 @@ play file info = simple LazyV.hPut (numberOfChannels numChannels)
     where numChannels = channels info
 
 numSeconds :: Info -> Double -> Int
-numSeconds info n = round $ n * fromIntegral (samplerate info) * fromIntegral (channels info)
+numSeconds info n = round $ n * fromIntegral (samplerate info) 
+                              * fromIntegral (channels info)
 
 playEffect :: t -> (t -> LazyV.Vector Double) -> String -> Info -> IO ()
 playEffect signal f msg info = do
@@ -185,4 +135,5 @@ reverse' info sig = append half (reverse half)
 
 clip' :: (Fractional a, RealFrac a1) => a1 -> a
 clip' a = round' a 1
-    where round' num digits = (fromInteger . round $ num * (10^digits)) / (10.0^^digits)
+    where round' num digits = (fromInteger . round $ num * (10^digits)) / 
+                                                           (10.0^^digits)
